c94bebb44d9893b69ae4c11b14554fe9738a445b
Cindymedina23 <117231241+Cindymedina23@users.noreply.github.com>
2022-11-09 19:12:11 -0400

    fix git

---
+++ modulos/registrar.php
@@ -0,0 +1,54 @@
+<?php
+
+$nombre=['nombre'];
+$apellido=['apellido'];
+$correo=['correo'];
+$clave=['clave'];
+
+?>
+
+<?php
+ 
+$enviar =  $_GET['enviar'] ?? '';
+
+if ($enviar=='enviar') {
+
+    include("modulos/ingresar.php");
+}
+
+?>
+
+<form  method="&_POST">
+
+<table  colspan="2"  cellspacing="2" cellspacing="10" width="10%">
+
+    <tr>
+        <td>nombre</td>
+        <td><input type="text" name="nombre"></td>
+    </tr>
+</br>
+    <tr>
+        <td>apellido</td>
+        <td><input type="text" name="apellido" ></td>
+    </tr>
+</br>
+    <tr>
+        <td>correo</td>
+        <td><input type="email" name="correo" ></td>
+    </tr>
+</br>
+    <tr>
+        <td>clave</td>
+        <td><input type="password" name="clave" ></td>
+    </tr>
+
+    <tr>
+        
+        <td><input type="submit"  value="enviar"></td>
+    </tr>
+
+
+</table>
+
+
+</form>
